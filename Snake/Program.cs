ff461f9984163a976bb578dd95cd72490d36df74
Anastasia <anastasia.n.0928@gmail.com>
2020-03-10 12:47:15 +0300

    Процедурный подход. Метод Draw

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,7 +6,21 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int x1 = 1;
+            //int y1 = 3;
+            //char sym1 = '*';
+            Draw(1, 3, '*');
+            //int x2 = 4;
+            //int y2 = 5;
+            //char sym2 = '#';
+            Draw(4, 5, '#');
+            Console.ReadLine();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
-}
+    }
+
