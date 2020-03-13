4ed21d5cf6d89a2c7bbe29a2264a22725d979065
Anastasia <anastasia.n.0928@gmail.com>
2020-03-14 00:59:06 +0300

    Создание конструктора

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,32 +6,15 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p = new Point();
-            p.x = 1;
-            p.x = 3;
-            p.sym = '*';
+            Point p = new Point(1, 3, '*');
             p.Draw();
 
-            Point p2 = new Point();
-            p2.x = 1;
-            p2.y = 3;
-            p2.sym = '#';
+            Point p2 = new Point(4, 5, '#');       
             p2.Draw();
-            //int x1 = 1;
-            //int y1 = 3;
-            //char sym1 = '*';
-            //Draw(1, 3, '*');
-            //int x2 = 4;
-            //int y2 = 5;
-            //char sym2 = '#';
-            //Draw(4, 5, '#');
+          
             Console.ReadLine();
         }
-        //static void Draw(int x, int y, char sym)
-        //{
-        //    Console.SetCursorPosition(x, y);
-        //    Console.Write(sym);
-        //}
+        
     }
     }
 
