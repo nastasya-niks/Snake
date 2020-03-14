3d9864ae668806a3a3979091d8e6a247f4809866
Anastasia <anastasia.n.0928@gmail.com>
2020-03-14 13:11:06 +0300

    Методы и память

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,15 +6,61 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p = new Point(1, 3, '*');
-            p.Draw();
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');       
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
-          
+
+            int x = 1;
+            Func1(x);
+            Console.WriteLine($"Func1 x={x}");
+
+            Func2(x);
+            Console.WriteLine($"Func2 x={x}");
+
+            Func3(x);
+            Console.WriteLine($"Func3 x={x}");
+
+            
+
+            Move(p1, 10, 10);
+            Console.WriteLine($"Move(p1, 10, 10) p1.x = {p1.x}, p1.y = {p1.y}");
+
+            Reset(p2);
+            Console.WriteLine($"Reset(p2) p2.x = {p2.x}, p2.y = {p2.y}");
+
             Console.ReadLine();
         }
-        
+        private static void Reset(Point p)
+        {
+            p = new Point();
+            //  Здесь переменная p2 останется без изменений.
+        }
+
+        private static void Move(Point p, int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y + dy;
+        }
+
+        private static void Func3(int x)
+        {
+            x++;
+            
+        }
+
+        private static void Func2(int val)
+        {
+            val++;
+        }
+
+        private static void Func1(int x)
+        {
+
+        }
     }
-    }
+
+}
+    
 
